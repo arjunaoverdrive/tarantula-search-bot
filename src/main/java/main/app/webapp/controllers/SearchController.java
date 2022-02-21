b1cbd8ae6d0f9243478187744399a2d70c35f580
Игорь Климов <igor1984syandex.ru>
2022-02-21 03:21:40 +0300

    fix 404

--- src/main/java/main/app/webapp/controllers/SearchController.java
+++ src/main/java/main/app/webapp/controllers/SearchController.java
@@ -5,11 +5,13 @@ import main.app.webapp.DTO.SearchDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class SearchController {
     @Autowired
     private final SearchService searchService;
