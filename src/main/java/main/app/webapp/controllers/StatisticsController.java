75859d688ea624cf9646c1817365329cffabd22c
Игорь Климов <igor1984syandex.ru>
2022-02-24 23:03:16 +0300

    add cross origin

--- src/main/java/main/app/webapp/controllers/StatisticsController.java
+++ src/main/java/main/app/webapp/controllers/StatisticsController.java
@@ -5,10 +5,12 @@ import main.app.webapp.DTO.StatisticsDtoWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
