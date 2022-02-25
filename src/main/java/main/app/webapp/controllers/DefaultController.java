0db94c5b0729f2b61abfd136ffe892f081c64658
Игорь Климов <igor1984syandex.ru>
2022-02-26 00:29:13 +0300

    fix localhost url in index.html

--- src/main/java/main/app/webapp/controllers/DefaultController.java
+++ src/main/java/main/app/webapp/controllers/DefaultController.java
@@ -3,9 +3,11 @@ package main.app.webapp.controllers;
 import main.app.services.SiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class DefaultController {
 
     private final SiteService siteService;
